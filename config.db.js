3e3d779513cca2d34977a31cd4687466894db6e1
miguel <miguelcaceda@gmail.com>
2024-01-16 08:39:33 +0100

    node express clase

---
+++ config.db.js
@@ -0,0 +1,16 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const mysql = require('mysql2');
+let connection;
+try {
+connection = mysql.createConnection({
+host: process.env.DBHOST,
+user: process.env.DBUSER,
+password: process.env.DBPASS,
+database: process.env.DBNAME,
+port: process.env.DBPORT
+});
+} catch (error) {
+console.log("Error al conectar con la base de datos");
+}
+module.exports = {connection};
